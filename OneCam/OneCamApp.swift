634e95ff3b40459c94824c0176b8e3c900419c9d
Gordon <me@gordonkirsch.dev>
2023-10-31 15:13:27 +0100

    Initial Commit

---
+++ OneCam/OneCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneCamApp.swift
+//  OneCam
+//
+//  Created by Gordon on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct OneCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
